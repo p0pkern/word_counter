0cfaaded674d658f58d302805568f3e83a2da233
p0pkern <45568242+p0pkern@users.noreply.github.com>
2020-09-08 06:48:09 -0700

    Adjusted chart name to reflect new chart style

--- wc_visual.py
+++ wc_visual.py
@@ -8,8 +8,9 @@ wc = WordCounter(filename, count)
 wc.word_counter()
 wc.adjust_values()
 
-radar_chart = pygal.Funnel()
-radar_chart.title = 'Word Count of ' + str(filename)
+# Save funnel chart to svg file.
+funnel_chart = pygal.Funnel()
+funnel_chart.title = 'Word Count of ' + str(filename)
 for key, value in wc.word_dict.items():
-    radar_chart.add(key, value)
-radar_chart.render_to_file('word_count.svg')
+    funnel_chart.add(key, value)
+funnel_chart.render_to_file('word_count.svg')
